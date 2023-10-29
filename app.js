5202973e47ee673f59443770d07346da5a7eaabb
kolawoleBenjamin <149281833+kolawoleBenjamin@users.noreply.github.com>
2023-10-29 02:43:03 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".text", {
+    strings: ["frontend Developer", "youtuber", "web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+// var typed = new Tyed(".text", {
+//     strings: ["frontend Developer", "youtuber", "web Developer"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// });
